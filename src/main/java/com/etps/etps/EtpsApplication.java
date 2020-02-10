8f25db35b96660ebdd76c630e29c11acc4745c1c
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-02-10 09:26:37 -0600

    first commit

---
+++ src/main/java/com/etps/etps/EtpsApplication.java
@@ -0,0 +1,13 @@
+package com.etps.etps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtpsApplication.class, args);
+    }
+
+}
