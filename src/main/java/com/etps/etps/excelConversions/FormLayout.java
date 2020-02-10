55505d0f239366fbf4552d49c9afaca8a7addcc4
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-02-10 14:28:03 -0600

    start on excel object

--- src/main/java/com/etps/etps/excelConversions/FormLayout.java
+++ src/main/java/com/etps/etps/excelConversions/FormLayout.java
@@ -1,5 +1,22 @@
 package com.etps.etps.excelConversions;
 
+import org.springframework.ui.Model;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 public class FormLayout {
+
+    public Map<String, Object[]> FormLayout(Model model, int id){
+        Map<String, Object[]> data = new TreeMap<String, Object[]>();
+
+
+
+
+
+        return data;
+    }
+
+//    public void generateObjectRow (Module module,)
 
 }
