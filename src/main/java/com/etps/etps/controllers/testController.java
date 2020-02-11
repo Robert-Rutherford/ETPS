6ef688bd673ac78e1dbc73bf5b249e82d28fe0ce
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2020-02-11 10:12:21 -0600

    added controller for test

---
+++ src/main/java/com/etps/etps/controllers/testController.java
@@ -0,0 +1,12 @@
+package com.etps.etps.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+    @GetMapping("/test")
+    public String testShow() {
+        return "testPage";
+    }
+}
