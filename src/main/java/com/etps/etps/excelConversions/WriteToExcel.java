cab00533f9a1567a9e08c4e909bc02a67fde8189
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-02-12 12:09:24 -0600

    Admin write excel

--- src/main/java/com/etps/etps/excelConversions/WriteToExcel.java
+++ src/main/java/com/etps/etps/excelConversions/WriteToExcel.java
@@ -1,7 +1,12 @@
 package com.etps.etps.excelConversions;
 
+import com.etps.etps.models.Campus;
+import com.etps.etps.models.Program;
+import com.etps.etps.models.Provider;
 import com.etps.etps.models.User;
-import com.etps.etps.repositories.Users;
+import com.etps.etps.repositories.Campuses;
+import com.etps.etps.repositories.Programs;
+import com.etps.etps.repositories.Providers;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -9,11 +14,10 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class WriteToExcel {
-    public void WriteToExcel(Map<String, Object[]> data, File outPath){
+    public void WriteExcel(Map<String, Object[]> data, File outPath){
 
         XSSFWorkbook workbook = new XSSFWorkbook();
 
@@ -46,6 +50,40 @@ public class WriteToExcel {
             e.printStackTrace();
         }
 
+    }
+
+    public Map<String, Object[]> GenerateUserData(User user, Providers providerDao, Campuses campusDao, Programs programsDao){
+        TreeMap<String, Object[]> data = new TreeMap<>();
+        data.put("1", new Object[] {"Provider ID", "Provider Name", "Provider Description","Campus ID","Campus Name",
+                "Program ID", "Program Name", "Program Description", "ETP ID"});
+        int treeNum = 2;
+        List<Provider> providers = new ArrayList<>();
+        if (user.isAdmin()){
+            providers = providerDao.findAll();
+
+        }else {
+            providers.add(providerDao.findById(user.getProvider().getId()));
+        }
+
+//        Provider provider = providerDao.findById(user.getProvider().getId());
+//        Provider provider = providerDao.findById(802);
+
+        for (Provider provider: providers) {
+            List<Campus> campusesList = campusDao.findAllByProvider_Id(provider.getId());
+
+            for (Campus campus: campusesList) {
+                List<Program> programsList = programsDao.findAllByCampus_Id(campus.getId());
+                for (Program program: programsList) {
+                    data.put(Integer.toString(treeNum),
+                            new Object[] {Long.toString(provider.getId()),provider.getProviderName(), provider.getDescription(),
+                                    campus.getId(), campus.getCampusName(),program.getId(),program.getName(),
+                                    program.getDescription(),program.getEtpCodeId()});
+                    treeNum++;
+                }
+            }
+        }
+
+        return data;
     }
 
 }
