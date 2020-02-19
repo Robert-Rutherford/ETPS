4285cbfc0cdd0c8d77e2d4e49dbf845adf62cb4f
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-02-19 16:54:57 -0600

    submission tables

--- src/main/java/com/etps/etps/controllers/SubmissionController.java
+++ src/main/java/com/etps/etps/controllers/SubmissionController.java
@@ -1,14 +1,21 @@
 package com.etps.etps.controllers;
 
 import com.etps.etps.excelConversions.StatusChange;
+import com.etps.etps.excelConversions.WriteToExcel;
+import com.etps.etps.models.DisplayData;
 import com.etps.etps.models.Message;
 import com.etps.etps.models.User;
 import com.etps.etps.repositories.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 public class SubmissionController {
@@ -30,7 +37,41 @@ public class SubmissionController {
 
 
     @GetMapping("/submission")
-    public String openSubmissionForm(){
+    public String openSubmissionForm(Model model){
+        User UserCheck = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User loggedInUser = userDao.findByUsername(UserCheck.getUsername());
+        WriteToExcel writeToExcel = new WriteToExcel(providerDao,campusDao,programDao,submissionDao);
+        Map<String, Object[]> approveData = writeToExcel.GenerateByStatus(loggedInUser,"approved");
+        Map<String, Object[]> pendingData = writeToExcel.GenerateByStatus(loggedInUser,"pending");
+        Map<String, Object[]> expiredData = writeToExcel.GenerateByStatus(loggedInUser,"expired");
+        ArrayList<DisplayData>  approvedList = new  ArrayList<>();
+        ArrayList<DisplayData>  pendingList = new  ArrayList<>();
+        ArrayList<DisplayData>  expiredList = new  ArrayList<>();
+
+        Set<String> keyset = approveData.keySet();
+        for (String key : keyset){
+            Object[] objArr = approveData.get(key);
+            DisplayData newData = new DisplayData(objArr);
+            approvedList.add(newData);
+        }
+        Set<String> keyset2 = pendingData.keySet();
+        for (String key : keyset2){
+            Object[] objArr = pendingData.get(key);
+            DisplayData newData = new DisplayData(objArr);
+            pendingList.add(newData);
+        }
+        Set<String> keyset3 = expiredData.keySet();
+        for (String key : keyset3){
+            Object[] objArr = expiredData.get(key);
+            DisplayData newData = new DisplayData(objArr);
+            expiredList.add(newData);
+        }
+
+        model.addAttribute("approvedList",approvedList);
+        model.addAttribute("pendingList",pendingList);
+        model.addAttribute("expiredList",expiredList);
+
+
         return "submissions";
     }
 
