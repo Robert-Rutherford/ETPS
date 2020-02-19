30d12e8daa08cc5871f9289672b1658cdb6edefc
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-02-19 10:26:19 -0600

    start of upload/download excel intergration

--- src/main/java/com/etps/etps/controllers/FileUploadController.java
+++ src/main/java/com/etps/etps/controllers/FileUploadController.java
@@ -1,26 +1,16 @@
 package com.etps.etps.controllers;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.etps.etps.excelConversions.ReadFromExcel;
 import com.etps.etps.models.User;
-import com.etps.etps.payload.Response;
 import com.etps.etps.repositories.*;
-//import com.etps.etps.services.FileStorageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.File;
+import java.io.IOException;
 
 
 @Controller
@@ -31,7 +21,7 @@ public class FileUploadController {
     private Campuses campusDao;
     private Programs programDao;
     private Submissions submissionDao;
-//    private FileStorageService fileStorageService;
+
 
     public FileUploadController(Users userDao, Providers providerDao, Campuses campusDao, Programs programDao, Submissions submissionDao) {
         this.userDao = userDao;
@@ -39,33 +29,23 @@ public class FileUploadController {
         this.campusDao = campusDao;
         this.programDao = programDao;
         this.submissionDao = submissionDao;
-//        this.fileStorageService = fileStorageService;
+
     }
 
 
     @PostMapping("/uploadFile")
     public String uploadFile(@RequestParam("file") MultipartFile file) {
-//        String fileName = fileStorageService.storeFile(file);
-
-//        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-//                .path("/downloadFile/")
-//                .path(fileName)
-//                .toUriString();
-
-//        Response loadedFile = new Response(fileName, fileDownloadUri,
-//                file.getContentType(), file.getSize());
 
         try {
 
-//            InputStream inputStream =  new BufferedInputStream(file.getInputStream());
-//            User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        ReadFromExcel readFromExcel = new ReadFromExcel(providerDao, campusDao, programDao, submissionDao);
-        File readFile = File.createTempFile("testFile",".xlsx");
-        file.transferTo(readFile);
+            User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            ReadFromExcel readFromExcel = new ReadFromExcel(providerDao, campusDao, programDao, submissionDao);
+            File readFile = File.createTempFile("testFile",".xlsx");
+            file.transferTo(readFile);
 
-        User loggedInUser = userDao.findByUserProviderId(900);
-        readFromExcel.ReadExcel(readFile,loggedInUser);
-        readFile.delete();
+//        User loggedInUser = userDao.findByUserProviderId(900);
+            readFromExcel.ReadExcel(readFile,loggedInUser);
+            readFile.delete();
 
     } catch (IOException e) {
         e.printStackTrace();
