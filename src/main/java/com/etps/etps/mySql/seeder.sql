fb7f2c1178c20a1b0f98a2dcf145340d3b26b269
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-06-02 14:22:00 -0500

    database extention: planning phase/setup

--- src/main/java/com/etps/etps/mySql/seeder.sql
+++ src/main/java/com/etps/etps/mySql/seeder.sql
@@ -10,6 +10,7 @@ INSERT INTO submissions (deadline, status) VALUES
 ('2020-01-01','approved');
 
 # providers table (MUST BE GENERATED FIRST)
+# need to add: provider url, address 1, addres 2
 INSERT INTO providers ( prov_id, provider_name,description, submission_id) VALUES
 (1, 'TWC', 'Admins of the TWC/ this column should never appear',null),
 (802,'ACCD Northwest Vista Collage','An Alamo College',1),
@@ -22,6 +23,7 @@ INSERT INTO campuses (camp_id, name, provider_id) VALUES
 (1496,'Main Campus',3);
 
 # programs
+# need to move: etp_code_id to table 11
 INSERT INTO programs ( prog_id, name,etp_code_id, description, campus_id) VALUES
 (8841,'Pharmacy technician','3164','Pharmacy Technician Fast Track Certificate',1),
 (8842,'Computer systems network','3174','AAS Network Administrator',1),
@@ -29,6 +31,54 @@ INSERT INTO programs ( prog_id, name,etp_code_id, description, campus_id) VALUES
 (8844,'Computer systems network','3172','Certificate Information Security Assurance',1),
 (8851,'Computer and Information','Netw0300','Network Administrator AAS',2),
 (8852,'Computer and Information','Comp0300','Computer Programing AAS',2);
+
+
+
+# new Tables
+# table 1: TWIST information
+# values: TWIST provider ID, TWIST Program ID, Action, program id (key)
+
+# table 2: provider address info
+# values: address 1, address 2, AddressCity, AddressState, AddressZipCode, County, provider ID (key)
+
+# table 3: admin contact info (provider)
+# values: institution type, Admin Contact Person, Admin Job Title,  Admin Phone, Admin Phone Extension,
+#           provider id (key)
+
+# table 4: financial contact info (provider)
+# values: Financial Aid Name, Financial aid phone, Financial Aid Email, provider id (key)
+
+# table 5: additional contact info (provider)
+# values: additonal contact person, additional contact job title, additional contact phone,
+#           additional contact phone extension, additional contact email, provider id (key)
+
+# table 6: ids and acks
+# values: THECBPrgmID, TEAPrgmID, otherPrgmID, FERPAAck, WIOAAck, OtherAck1, OtherAck2, OtherAck3, OtherAck4,
+#           provider id (key)
+
+# table 7: campus address info
+# values: address 1, address 2, campus city, campus state, campus zip code, campus county, campus id (key)
+
+# table 8: campus contact info
+# values: campus contact Name, campus contact job title, campus phone, campus phone extension, campus email,
+#           campus id (key)
+
+# table 9: campus additional contact info
+# values: additional contact name, additional contact jonb title, additional contact phone, phone extension, email,
+#           campus id (key)
+
+# table 10: campus additional info
+# values: info public transit, on sit child care, flexible hours,
+
+# table 11: program additional info
+# values: CSCPrgmID, ETPCodeID, CIPCode, PellEligble, PreRequisites, programURL, program outcome, Associate Credit Name,
+#           length contact hours, length weeks, program format, ONET1, ONET2, ONET3, student data,
+#           number of apprentices, program id (key)
+
+# table 12: Tuition and costs
+# values: cost required Tuition fee description, costReq booksSupplies, book supplies description, CostOptOther,
+#           costOptOtherDescription, ETP Report type, Out of District Cost Req tuition fee,
+#           Out of District Cost Req tuition fee description, program id (key)
 
 
 # users table
