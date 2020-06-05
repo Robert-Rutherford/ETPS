9ddd167a1225244195118f897710334366fc9432
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-06-05 13:42:58 -0500

    database extention: generate models (3/4) -- Campus address, Campus Contact, Campus additional contact

---
+++ src/main/java/com/etps/etps/models/CampusAdditionalContact.java
@@ -0,0 +1,84 @@
+package com.etps.etps.models;
+//# table 9: campus additional contact info
+//# values: additional contact name, additional contact jonb title, additional contact phone, phone extension, email,
+//#           campus id (key)
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "campusAdditionalContact")
+public class CampusAdditionalContact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    private Provider provider;
+
+    private String name;
+    private String jobTitle;
+
+    private String phone;
+    private String phoneExtension;
+    private String email;
+
+    public CampusAdditionalContact() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhoneExtension() {
+        return phoneExtension;
+    }
+
+    public void setPhoneExtension(String phoneExtension) {
+        this.phoneExtension = phoneExtension;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
